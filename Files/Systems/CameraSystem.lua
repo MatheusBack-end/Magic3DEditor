1c96c2643f842a245a570ecae12fd8ceb07ecf74
iMatthhzCode <matheunf2006@gmail.com>
2023-08-05 16:40:45 -0300

    first update

--- Files/Systems/CameraSystem.lua
+++ Files/Systems/CameraSystem.lua
@@ -14,18 +14,47 @@ function start()
   camera_component = camera:findComponent("Camera")
 end
 
+function get_vertices_from_triangle(triangle, vertices)
+  local v = luajava.newInstance("java.util.ArrayList")
+
+  v:add(vertices:get(triangle:getX()))
+  v:add(vertices:get(triangle:getY()))
+  v:add(vertices:get(triangle:getZ()))
+
+  return v
+end
+
+function vertices_is_equals(other, vertices, triangles)
+  for i=0, triangles:size() -1, 1 do
+    local v = get_vertices_from_triangle(triangles:get(i), vertices)
+    
+    local e = true
+    for s=0, 2, 1 do
+      if v:get(s) ~= other:get(s) then
+        e = false
+      end
+    end
+
+    if e then
+      return i
+    end
+  end
+end
+
 function update()
   if Input:getTouch(0):isDown() then
     local hit = laser:trace(camera:getGlobalPosition(), 
       camera_component:screenPointNormal(Input:getTouchPosition(0)),
       100.5)
 
-    Console:log(hit ~= nil)
-
     if hit ~= nil then
-      local tst = "c"--hit:getCollider():getVertex():getVertices():get(0)
+      local object = hit:getObject()
+      local object_vertex = object:findComponent("ModelRenderer"):getVertex()
+      local object_vertices = object_vertex:getVertices()
+      local object_triangles = object_vertex:getTriangles()
+      local collider_vertices = hit:getCollider():getVertex():getVertices()
 
-      Console:log(tst)
+      Console:log(vertices_is_equals(collider_vertices, object_vertices, object_triangles))
     end
   end
 
