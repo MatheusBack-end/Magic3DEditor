c345f95b50084793c5b43e0b2a7e47343de92bf5
iMatthhzCode <matheunf2006@gmail.com>
2023-08-07 17:52:53 -0300

    wireframe update!

--- Files/Systems/WireframeGenerator.lua
+++ Files/Systems/WireframeGenerator.lua
@@ -31,17 +31,28 @@ function add_vertice(v)
 end
 
 function draw_line(A, B)
+  --TODO: codigo lixo, refatorar ao longo do projeto
   local s = 0.003
 
-  local va = add_vertice(vector3(A:getX() + s, A:getY(), A:getZ()))
-  local vb = add_vertice(vector3(A:getX() - s, A:getY(), A:getZ()))
-  local vc = add_vertice(vector3(B:getX() + s, B:getY(), B:getZ()))
-  local vd = add_vertice(vector3(B:getX() - s, B:getY(), B:getZ()))
-  
+  local va = add_vertice(vector3(A:getX() + s, A:getY() + s, A:getZ()))
+  local vb = add_vertice(vector3(A:getX() - s, A:getY() + s, A:getZ()))
+  local vc = add_vertice(vector3(B:getX() + s, B:getY() + s, B:getZ()))
+  local vd = add_vertice(vector3(B:getX() - s, B:getY() + s, B:getZ()))
+  local ve = add_vertice(vector3(A:getX() + s, A:getY() - s, A:getZ()))
+  local vf = add_vertice(vector3(B:getX() + s, B:getY() - s, B:getZ()))
+  local vg = add_vertice(vector3(A:getX() - s, A:getY() - s, A:getZ()))
+  local vh = add_vertice(vector3(B:getX() - s, B:getY() - s, B:getZ()))
+
   frame_triangles:add(vector3(va, vb, vc))
   frame_triangles:add(vector3(vb, vd, vc))
+  frame_triangles:add(vector3(vf, vg, ve))
+  frame_triangles:add(vector3(vf, vh, vg))
   frame_triangles:add(vector3(vc, vb, va))
   frame_triangles:add(vector3(vc, vd, vb))
+  frame_triangles:add(vector3(vf, ve, va))
+  frame_triangles:add(vector3(vc, vf, va))  
+  frame_triangles:add(vector3(vb, vg, vh))
+  frame_triangles:add(vector3(vb, vh, vd))
 end
 
 function generate()
@@ -54,13 +65,19 @@ function generate()
     make_wireframe_from_face(model_triangles:get(i))
   end
 
+  --draw_line(vector3(0,0,0), vector3(0,0,0.8))
   local new_model_renderer = ModelRenderer:new()
   local new_vertex = Vertex:new()
+  local color = Color:new(0,0,0)
+  color:setIntRed(0)
+  color:setIntGreen(0)
+  color:setIntBlue(0)
   new_vertex:setVertices(frame_vertices)
   new_vertex:setTriangles(frame_triangles)
   new_model_renderer:setVertex(new_vertex)
   new_model_renderer:setMaterial(Material:new())
   myObject:addComponent(new_model_renderer)
+  new_model_renderer:getMaterial():setColor("diffuse", color)
 end
 
 function start()
